82b9273c2678d0a6b2678c957a8aa8271a4af07f
Akkipr <akshayanpraba6@gmail.com>
2025-02-08 04:25:15 -0500

    miscellaneous structs

---
+++ htn/Objects.swift
@@ -0,0 +1,16 @@
+//
+//  Objects.swift
+//  htn
+//
+//  Created by Akshayan on 2024-09-14.
+//
+
+import SwiftUI
+
+struct Objects: Identifiable {
+    var id: String
+    var specificDiscount: String
+    var dstion: String
+    var Link: URL
+    var photoUrl: String
+}
---
+++ htn/Objects2.swift
@@ -0,0 +1,17 @@
+//
+//  Objects2.swift
+//  htn
+//
+//  Created by Akshayan on 2024-09-15.
+//
+
+import SwiftUI
+
+
+struct Objects2: Identifiable {
+    var id: String
+    var name: String
+    var detion: String
+    var linkUrl: URL
+    var photo: String
+}
